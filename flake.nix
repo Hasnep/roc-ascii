41332dc21d6df28a2ae77959acd162c54a049cc8
Hannes <h@nnes.dev>
2024-07-24 20:40:46 +0800

    Replace devenv with flake-parts

--- flake.nix
+++ flake.nix
@@ -1,57 +1,48 @@
 {
   inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-    systems.url = "github:nix-systems/default";
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     roc.url = "github:roc-lang/roc?rev=f7011c8e33b92a7e9f809e6f16849de9fcfa8dd7";
   };
 
   nixConfig = {
-    extra-trusted-public-keys = [
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      "roc-lang.cachix.org-1:6lZeqLP9SadjmUbskJAvcdGR2T5ViR57pDVkxJQb8R4="
-    ];
-    extra-trusted-substituters = ["https://devenv.cachix.org" "https://roc-lang.cachix.org"];
+    extra-trusted-public-keys = "roc-lang.cachix.org-1:6lZeqLP9SadjmUbskJAvcdGR2T5ViR57pDVkxJQb8R4=";
+    extra-trusted-substituters = "https://roc-lang.cachix.org";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    devenv,
-    systems,
-    roc,
-    ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    packages = forEachSystem (system: {
-      devenv-up = self.devShells.${system}.default.config.procfileScript;
-    });
-
-    devShells =
-      forEachSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            {
-              # https://devenv.sh/reference/options/
-              name = "roc-ascii";
-              packages = [
-                roc.packages.${system}.cli
-                pkgs.just
-                pkgs.pre-commit
-                pkgs.alejandra
-              ];
-              enterShell = "pre-commit install";
-            }
-          ];
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      roc,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      perSystem =
+        {
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            name = "roc-ascii";
+            packages = [
+              inputs'.roc.packages.cli
+              pkgs.just
+              pkgs.pre-commit
+            ];
+            enterShell = "pre-commit install --overwrite";
+          };
+          formatter = pkgs.nixfmt-rfc-style;
         };
-      });
-  };
+    };
 }
